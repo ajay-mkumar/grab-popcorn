982b452776d36ecf4d601df0c131a3387c1f8ec2
ajay muthukumaran <65906852+ajay-mkumar@users.noreply.github.com>
2025-04-24 12:58:38 +0530

    initial commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+
+function Logo() {
+    return (
+      <div className="logo">
+        <span role="img">🍿</span>
+        <h1>Grab Popcorn</h1>
+      </div>
+    );
+  }
+
+
+  export default Logo;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import Logo from "./Logo";
+
+function NavBar({ children }) {
+
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      {children}
+    </nav>
+  );
+}
+
+
+
+
+
+
+export default NavBar;
